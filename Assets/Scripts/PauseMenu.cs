4c0b95fd342c90b6760cf7043493a12fc805060d
GGMendel <130412813+GGMendel@users.noreply.github.com>
2023-09-20 21:44:38 -0300

    Menu e Pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private bool isPaused;
+    public GameObject pausePanel;
+    public string cena;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape))
+       {
+            PauseScreen();
+       } 
+
+    }
+
+    void PauseScreen()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+            pausePanel.SetActive(false);
+        }
+        else
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+            pausePanel.SetActive(true);
+        }
+    }
+
+    public void VoltarMenu()
+    {
+        SceneManager.LoadScene(cena);
+    }
+}
